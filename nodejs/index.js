59e959be687e13a1e5ff43aeebf1138113bb644e
Atlas-lili <3163299800@qq.com>
2019-10-12 16:48:53 +0800

    GZIP调整

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,13 +1,11 @@
 const Koa = require('koa')   //koa2中间件依赖
 const static = require('koa-static')   //静态资源服务插件
-const compress = require('koa-compress'); //开启GZIP压缩
 const path = require('path')           //路径管理
 
 var port = (process.env.NODE_ENV==='production')? 80 : 3000;
 const staticPath = './assets'
 
 const app = new Koa()       //js的继承
-app.use(compress());
 app.use(static(
     path.join( __dirname, staticPath)
 ))
